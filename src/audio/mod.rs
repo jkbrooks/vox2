2c83fb4ca32869f0b8df2cc941d3ecae2cc8e98b
Jazzy <jazear.brooks@gmail.com>
2025-08-11 06:07:52 +0000

    Implement play_sound function

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -2,4 +2,6 @@ pub enum AudioFormat {
     MP3,
     WAV,
     OGG,
+}pub fn play_sound(sound_name: &str, format: AudioFormat) {
+    println!("Playing sound {} with format {:?}", sound_name, format);
 }
