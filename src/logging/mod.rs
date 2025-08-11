002db82a4c90f082c4dca422a56b3ac63e7d289c
Jazzy <jazear.brooks@gmail.com>
2025-08-11 07:18:09 +0000

    Add LogLevel enum definition

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -1,13 +1,6 @@
-pub mod logger {
-    use std::fs::OpenOptions;
-    use std::io::Write;
-
-    pub fn log(message: &str) {
-        let mut file = OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open("log.txt")
-            .unwrap();
-        writeln!(file, "{}", message).unwrap();
-    }
+pub enum LogLevel {
+    Debug,
+    Info,
+    Warning,
+    Error,
 }
