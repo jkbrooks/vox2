c744d879040081c3ae5ed347b319430c0737907c
Jazzy <jazear.brooks@gmail.com>
2025-08-11 07:18:09 +0000

    Add Logger struct and methods implementation

--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -4,3 +4,32 @@ pub enum LogLevel {
     Warning,
     Error,
 }
+pub struct Logger {
+    level: LogLevel,
+}
+
+impl Logger {
+    pub fn new(level: LogLevel) -> Self {
+        Logger { level }
+    }
+
+    pub fn log(&self, level: LogLevel, message: &str) {
+        // Logging logic here
+    }
+
+    pub fn debug(&self, message: &str) {
+        self.log(LogLevel::Debug, message);
+    }
+
+    pub fn info(&self, message: &str) {
+        self.log(LogLevel::Info, message);
+    }
+
+    pub fn warn(&self, message: &str) {
+        self.log(LogLevel::Warning, message);
+    }
+
+    pub fn error(&self, message: &str) {
+        self.log(LogLevel::Error, message);
+    }
+}
